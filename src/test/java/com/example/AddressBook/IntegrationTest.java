49585890b4e37027a71b85588b088873bb176012
jamildergham <jamildergham@cmail.carleton.ca>
2023-02-08 17:11:31 -0500

    second commit :)

---
+++ src/test/java/com/example/AddressBook/IntegrationTest.java
@@ -0,0 +1,36 @@
+package com.example.AddressBook;
+
+
+import static org.hamcrest.Matchers.containsString;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import jdk.jfr.Enabled;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class IntegrationTest {
+
+
+        @Autowired
+        private MockMvc mockMvc;
+
+        @MockBean
+        private AddressBookRepository addressBookRepository;
+
+        @Test
+        public void greetingShouldReturnMessageFromService() throws Exception {
+            this.mockMvc.perform(get("/Landing")).andDo(print()).andExpect(status().isOk());
+        }
+}
